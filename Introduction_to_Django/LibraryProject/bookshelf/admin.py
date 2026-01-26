982002b61a93776cd2a49037c7b6525b43ec19d9
Elvis_Kusi <135480109+HHH880@users.noreply.github.com>
2026-01-26 17:58:16 -0200

    Update admin.py

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import Book
 
 class BookAdmin(admin.ModelAdmin):
-    list_display = ("title", "author")
+    list_display = ("title", "author", "publication_year")
+    list_filter = ("publication_year",)
 
 admin.site.register(Book, BookAdmin)
